6978b371bddf2a9d355f1db20a6312fa3932a95e
Francisco <fcomontsep@gmail.com>
2025-10-08 17:10:52 -0300

    Actualizo: Instalación de Flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	darkMode: "class",
+	content: ["./index.html", ".//**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
+	theme: {
+		extend: {},
+	},
+	plugins: [require("flowbite/plugin")],
+};
