876d1ff11b6934b5257e0925f47230480c7cd0a1
Francisco <fcomontsep@gmail.com>
2025-10-14 13:03:05 -0300

    Remuevo: CSS no utilizado

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	darkMode: "class",
-	content: ["./index.html", ".//**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
-	theme: {
-		extend: {},
-	},
-	plugins: [require("flowbite/plugin")],
-};
